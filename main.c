a6da48a12fac27f64d14745deae09de426ba823d
jbergos <jbergos@jbergos.myguest.virtualbox.org>
2025-10-06 19:12:17 +0200

    some modif

--- main.c
+++ main.c
@@ -10,27 +10,45 @@
 
 #define PACKET_SIZE 64
 
-uint16_t icmp_checksum(const void *buf, size_t len) {
-    const uint8_t *bytes = buf;
+uint16_t icmp_checksu(void *buf, int len) {
+    uint8_t *bytes = buf;
     uint32_t sum = 0;
 
     while (len > 1) {
-        sum += (uint16_t)bytes[0] << 8 | bytes[1];
+        sum += (uint16_t)*bytes << 8 | *(bytes + 1);
         bytes += 2;
         len -= 2;
     }
 
     if (len == 1) {
-        sum += (u_int16_t)bytes[0] << 8;
+        sum += (u_int16_t)*bytes << 8;
     }
 
     while (sum >> 16) {
         sum = (sum & 0xFFFF) + (sum >> 16);
     }
 
-    return (uint16_t)(~sum);
+    return (u_int16_t)(~sum);
 }
 
+uint16_t icmp_checksum(void *buf, int len) {
+    uint16_t *bytes = buf;
+    uint32_t sum = 0;
+
+    while (len > 1) {
+        sum += *bytes;
+        bytes++;
+        len -= 2;
+    }
+
+    if (len == 1)
+        sum += *bytes;
+
+    while (sum >> 16) {
+        sum = (sum & 0xFFFF) + (sum >> 16);
+    }
+    return (uint16_t)~sum;
+}
 int main() {
     int sock;
     struct sockaddr_in addr;
@@ -54,7 +72,7 @@ int main() {
     icmp->un.echo.id = getpid() & 0xFFFF;
     icmp->un.echo.sequence = 1;
     icmp->checksum = 0;
-    icmp->checksum = icmp_checksum(packet, sizeof(struct icmphdr));
+    icmp->checksum = icmp_checksum(&packet, sizeof(struct icmphdr));
 
     if (sendto(sock, packet, sizeof(struct icmphdr), 0,
             (struct sockaddr *)&addr, sizeof(addr)) < 0) {
@@ -75,7 +93,7 @@ int main() {
     icmp = (struct icmphdr *)packet;
     uint16_t recv_checksum = icmp->checksum;
     icmp->checksum = 0;
-    uint16_t calc_checksum = icmp_checksum(packet, sizeof(struct icmphdr));
+    uint16_t calc_checksum = icmp_checksum(&packet, sizeof(struct icmphdr));
 
     if (recv_checksum == calc_checksum) {
         printf("Checksum correct \n");
