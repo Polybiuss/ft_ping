b2cc35e605408464abd26dbcbc65f46cabae85ca
jbergos <jbergos@jbergos.myguest.virtualbox.org>
2025-10-15 16:16:49 +0200

    almost finish doc

--- src/main.c
+++ src/main.c
@@ -6,5 +6,12 @@ int main(int argc, char *argv[]) {
     if (argc == 1){
         printf("%s: %s\n", NAME_PROG, MISS_H_OPE);
     }
+    // for (int i = 0; i < argc; i++)
+    // {
+    //     printf("%d: %s\n", i, argv[i]);
+    // }
+    else {
+        
+    }
     return (0);
 }
